04d3d9b4e686b7b0541076301db7930cf14c260d
Elsti97 <elsti.shehati@hotmail.it>
2023-03-16 14:14:23 +0100

    Snack 1 done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿snack1();
+
+void snack1()
+{ 
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    int maggiore = num1 > num2 ? num1 : num2;
+
+    if (num1 == num2)
+    {
+        Console.WriteLine("Numeri uguali");
+    }
+    else
+    {
+        Console.WriteLine("il maggiore è: " + maggiore);
+    }
+}
