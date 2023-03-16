192ae28ed559dbce53708d48e24a89f9c8652353
Elsti97 <elsti.shehati@hotmail.it>
2023-03-16 14:21:07 +0100

    Snack 2 done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿snack1();
+﻿//snack1();
+snack2();
 
 void snack1()
 { 
@@ -14,5 +15,26 @@ void snack1()
     else
     {
         Console.WriteLine("il maggiore è: " + maggiore);
+    }
+}
+
+
+void snack2() 
+{
+    Console.WriteLine("Inserisci una parola");    
+    string str1 = Console.ReadLine() ?? "";
+
+    Console.WriteLine("Inserisci un'altra parola");
+    string str2 = Console.ReadLine() ?? "";
+
+    if (str1.Length < str2.Length)
+    {
+        Console.WriteLine("La parola più corta è: " + str1);
+        Console.WriteLine("La parola più lunga è: " + str2);
+    }
+    else
+    {
+        Console.WriteLine("La parola più corta è: " + str2);
+        Console.WriteLine("La parola più lunga è: " + str1);
     }
 }
