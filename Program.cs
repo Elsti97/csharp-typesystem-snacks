ce13efb10c37c43dabdc081d19ed36cd2fd3807d
Elsti97 <elsti.shehati@hotmail.it>
2023-03-16 17:33:00 +0100

    Bonus done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,60 @@
-﻿//snack1();
+﻿
+bool exit = false;
+
+while (!exit)
+{
+    Console.WriteLine("Inserisci il numero dello snack da eseguire da 1 a 12 oppure digita 'esci' per terminare il programma");
+    string input = Console.ReadLine() ?? "";
+    Console.WriteLine();
+
+    switch (input)
+    {
+        case "1":
+            snack1();
+            break;
+        case "2":
+            snack2();
+            break;
+        case "3":
+            snack3();
+            break;
+        case "4":
+            snack4();
+            break;
+        case "5":
+            snack5();
+            break;
+        case "6":
+            snack6();
+            break;
+        case "7":
+            snack7();
+            break;
+        case "8":
+            snack8();
+            break;
+        case "9":
+            snack9();
+            break;
+        case "10":
+            snack10();
+            break;
+        case "11":
+            snack11();
+            break;
+        case "12":
+            snack12();
+            break;
+        case "esci":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Errore, riprova");
+            break;
+    }
+}
+
+//snack1();
 //snack2();
 //snack3();
 //snack4();
@@ -8,12 +64,15 @@
 //snack8();
 //snack9();
 //snack10();
-snack11();
+//snack11();
 //snack12();
 
 void snack1()
-{ 
+{
+    Console.WriteLine("1)Scrivi 2 numeri, stamperò il maggiore");
+    Console.WriteLine("Inserisci un numero: ");
     int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Inserisci un altro numero: ");
     int num2 = Convert.ToInt32(Console.ReadLine());
 
     int maggiore = num1 > num2 ? num1 : num2;
@@ -26,12 +85,14 @@ void snack1()
     {
         Console.WriteLine("il maggiore è: " + maggiore);
     }
+    Console.WriteLine();
 }
 
 
-void snack2() 
+void snack2()
 {
-    Console.WriteLine("Inserisci una parola");    
+    Console.WriteLine("2)Scrivi 2 parole, le metterò in ordine di grandezza");
+    Console.WriteLine("Inserisci una parola");
     string str1 = Console.ReadLine() ?? "";
 
     Console.WriteLine("Inserisci un'altra parola");
@@ -47,11 +108,13 @@ void snack2()
         Console.WriteLine("La parola più corta è: " + str2);
         Console.WriteLine("La parola più lunga è: " + str1);
     }
+    Console.WriteLine();
 }
 
 
 void snack3()
 {
+    Console.WriteLine("3)Scrivi 10 numeri, farò la somma");
     int sum = 0;
     for (int i = 1; i <= 10; i++)
     {
@@ -60,11 +123,13 @@ void snack3()
         sum += num;
     }
     Console.WriteLine("La somma totale è: " + sum);
+    Console.WriteLine();
 }
 
 
 void snack4()
 {
+    Console.WriteLine("4)Ecco la somma dei numeri da 2 a 10");
     int sum = 0;
     int min = 2;
     int max = 10;
@@ -78,11 +143,13 @@ void snack4()
     double avg = sum / nums;
     Console.WriteLine("La somma è: " + sum);
     Console.WriteLine("La media è: " + avg);
+    Console.WriteLine();
 }
 
 
 void snack5()
 {
+    Console.WriteLine("5)Inserisci un numero, se è pari lo accetto, se dispari lo trasformerò nel numero dopo");
     Console.Write("Inserisci un numero: ");
     int num = Convert.ToInt32(Console.ReadLine());
 
@@ -94,11 +161,13 @@ void snack5()
     {
         Console.WriteLine("Il numero inserito è dispari, ecco il numero pari subito dopo: " + (num + 1));
     }
+    Console.WriteLine();
 }
 
 
 void snack6()
 {
+    Console.WriteLine("6)Scrivi il tuo nome, dirò se sei stato invitato alla festa");
     string[] guestList = { "Aldo", "Giovanni", "Giacomo", "Parrego", "Nagasella", "Nagheso", "Pasaghella", "Parrassanna", "Franco" };
 
     Console.Write("Inserisci il tuo nome: ");
@@ -112,11 +181,13 @@ void snack6()
     {
         Console.WriteLine("Non sei stato invitato");
     }
+    Console.WriteLine();
 }
 
 
 void snack7()
 {
+    Console.WriteLine("7)Scrivi 6 numeri, alla fine ti dirò quali sono dispari");
     int[] oddArray = new int[0];
 
     for (int i = 0; i < 6; i++)
@@ -144,11 +215,15 @@ void snack7()
     {
         Console.Write(odd + " ");
     }
+
+    Console.WriteLine();
+    Console.WriteLine();
 }
 
 
 void snack8()
 {
+    Console.WriteLine("8)Ecco la somma dei numeri in posizione dispari");
     int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int oddSum = 0;
 
@@ -158,11 +233,13 @@ void snack8()
     }
 
     Console.WriteLine("La somma dei numeri in posizione dispari è: " + oddSum);
+    Console.WriteLine();
 }
 
 
 void snack9()
 {
+    Console.WriteLine("9)Scrivi un numero, te li chiederò fino alla somma di 50");
     int[] numArray = new int[0];
     int sum = 0;
 
@@ -191,13 +268,14 @@ void snack9()
     }
 
     Console.WriteLine("La somma ha raggiunto 50, il risultato è: " + sum);
+    Console.WriteLine();
 }
 
 
 
 void snack10()
 {
-
+    Console.WriteLine("10)Scrivi un numero, creerò N array con numeri casuali");
     Console.WriteLine("Inserisci un numero: ");
 
     int N = Convert.ToInt32(Console.ReadLine());
@@ -217,14 +295,15 @@ void snack10()
         {
             Console.Write($"{num} ");
         }
-
         Console.WriteLine();
     }
+    Console.WriteLine();
 }
 
 
 void snack11()
 {
+    Console.WriteLine("11)Scrivi 2 parole, le stamperò se sono uguali o (se diverse) la più lunga");
     Console.Write("Inserisci la prima parola: ");
     string str1 = Console.ReadLine() ?? "";
 
@@ -240,10 +319,24 @@ void snack11()
         string longer = str1.Length > str2.Length ? str1 : str2;
         Console.WriteLine("La parola più lunga è: " + longer);
     }
+    Console.WriteLine();
 }
 
 
 void snack12()
 {
+    Console.WriteLine("12)Scrivi un numero, ti dirò se è pari o dispari");
+    Console.Write("Inserisci un numero: ");
+    int num = Convert.ToInt32(Console.ReadLine());
 
+    if (num % 2 == 0)
+    {
+        Console.WriteLine(num + " è un numero pari");
+    }
+    else
+    {
+        Console.WriteLine(num + " è un numero dispari");
+    }
+    Console.WriteLine();
 }
+
